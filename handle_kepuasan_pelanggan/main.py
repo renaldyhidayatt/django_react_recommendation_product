697620a78c1621454e4e3ec93d6b567cd4d88d3f
renaldyhidayatt <renaldyhidayatt@gmail.com>
2023-11-15 06:30:28 +0700

    Adding Handling Kepuasaan pelanggan

--- handle_kepuasan_pelanggan/main.py
+++ handle_kepuasan_pelanggan/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import random
+import matplotlib.pyplot as plt
 
 url = 'https://t67y.egoq.lyr.id/api/dashboard'
 
@@ -19,11 +20,21 @@ if response.status_code == 200:
 
    
     df_pendapatan = pd.DataFrame({
-        'Bulan': pd.date_range(start='2023-11-01', periods=12, freq='M'),
+        'Bulan': pd.date_range(start='2023-01-01', periods=12, freq='M'),
         'Pendapatan': data["data"]["pendapatan"]
     })
    
-    st.line_chart(df_pendapatan.set_index('Bulan'))
+ 
+    plt.figure(figsize=(10, 6))
+    plt.plot(df_pendapatan['Bulan'], df_pendapatan['Pendapatan'])
+    plt.xlabel('Bulan')
+    plt.ylabel('Pendapatan')
+    plt.title('Grafik Pendapatan')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+
+   
+    st.pyplot(plt)
 else:
     st.write(f"Failed to retrieve data. Status code: {response.status_code}")
 
@@ -35,32 +46,51 @@ st.header('Kepuasan Pengguna')
 st.bar_chart(df_kepuasan.set_index('status_kepuasan')['persentase'])
 
 
-# Pendapatan Tambahan
-st.header('Pendapatan Tambahan')
-st.write('Total Pendapatan Tambahan:', data["data"]["totalPendapatan"])
+
+st.header('Pertumbuhan Triwulan')
+df_order = pd.read_csv('./dataset/modified_order.csv')
+
+df_order['created_at'] = pd.to_datetime(df_order['created_at'])
 
 
+df_order = df_order[
+    (df_order['created_at'].dt.year == 2023) & 
+    (df_order['created_at'].dt.quarter <= 4)  
+]
 
-data_pertumbuhan = {
-    'Triwulan': [],
-    'Persentase Pertumbuhan': []
-}
-
-
-for triwulan in range(1, 5):
-    for _ in range(40):
-        data_pertumbuhan['Triwulan'].append(f'Triwulan {triwulan}')
-        data_pertumbuhan['Persentase Pertumbuhan'].append(random.randint(10 + (triwulan - 1) * 5, 10 + triwulan * 5))
-
-df_pertumbuhan = pd.DataFrame(data_pertumbuhan)
+transactions_per_quarter = df_order.groupby(df_order['created_at'].dt.to_period("Q")).size()
 
 
-st.header('Pertumbuhan Industri E-commerce')
-st.bar_chart(df_pertumbuhan.set_index('Periode'))
+fig, ax = plt.subplots(figsize=(8, 6))
+colors = ['skyblue', 'salmon', 'lightgreen', 'goldenrod']
+quarters = ['Triwulan 1', 'Triwulan 2', 'Triwulan 3', 'Triwulan 4']
+transactions_per_quarter.plot(kind='bar', color=colors, ax=ax)
+ax.set_title('Jumlah Transaksi per Triwulan (Triwulan 1-4)')
+ax.set_xlabel('Triwulan')
+ax.set_ylabel('Persentase Pertumbuhan')
+ax.set_xticks(range(len(quarters)))
+ax.set_xticklabels(quarters, rotation=45)
+ax.grid(axis='y')
+
+st.pyplot(fig)
 
 
 
 df_brand = pd.read_csv('./dataset/brand.csv')
 
+grouped = df_brand.groupby('Brand Recognition')['Perubahan'].mean()
+
 st.header('Peningkatan Branding Platform E-commerce')
-st.bar_chart(df_brand.set_index('Brand Recognition'))
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.bar(grouped.index, grouped.values)
+ax.set_xlabel('Brand')
+ax.set_ylabel('Rata-rata Perubahan')
+ax.set_title('Peningkatan Branding Platform E-commerce')
+ax.set_xticklabels(grouped.index, rotation=45)
+
+st.pyplot(fig)
+
+
+
